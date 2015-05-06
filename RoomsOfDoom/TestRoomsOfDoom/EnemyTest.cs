c59921dc82990961feb7b58a25da678414a3595c
Sam de Redelijkheid <sam_mie_sam@hotmail.com>
2015-05-06 20:04:37 +0200

    Files.

---
+++ RoomsOfDoom/TestRoomsOfDoom/EnemyTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RoomsOfDoom;
+
+namespace TestRoomsOfDoom
+{
+    [TestClass]
+    public class EnemyTest : HittableTest
+    {
+
+        public EnemyTest() : base()
+        {
+
+        }
+
+        public override IHittable getHittable()
+        {
+            return new Enemy("test", 100);
+        }
+    }
+}
