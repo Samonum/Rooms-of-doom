a0ffa0158f6536ecdd06aacac915341ce8556a63
Chrit Hameleers <chrithameleers@hotmail.com>
2015-05-07 16:08:48 +0200

    moar code coverage woop tee doo

--- RoomsOfDoom/TestRoomsOfDoom/EnemyTest.cs
+++ RoomsOfDoom/TestRoomsOfDoom/EnemyTest.cs
@@ -17,5 +17,27 @@ namespace TestRoomsOfDoom
         {
             return new Enemy("test",'T', 100);
         }
+
+        [TestMethod]
+        public void EnemySetterTest()
+        {
+            //Arrange
+            Random r = new Random();
+            MonsterCreator M = new MonsterCreator(r,20);
+            Enemy e = M.CreateMonster(1);
+            //act
+            e.CurrentHP = 1;
+            for (int i = 0; i < 100; i++)
+            {
+                e.CurrentHP++;
+            }
+
+            Assert.IsTrue(e.MaxHP >= e.CurrentHP);
+
+            e.MaxHP = 100;
+            char c = e.Glyph;
+            e.Alive = false;
+
+        }
     }
 }
