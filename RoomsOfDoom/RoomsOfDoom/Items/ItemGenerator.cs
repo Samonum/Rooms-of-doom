6429505e3305fd8c8c043d5e9c78b24e3ce3829b
Sam de Redelijkheid <sam_mie_sam@hotmail.com>
2015-05-08 14:13:50 +0200

    That

---
+++ RoomsOfDoom/RoomsOfDoom/Items/ItemGenerator.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RoomsOfDoom.Items
+{
+    public class ItemGenerator
+    {
+        Random r;
+        Dungeon dungeon;
+        public ItemGenerator(Dungeon dungeon, Random r)
+        {
+            r = new Random();
+            dungeon = dungeon;
+        }
+
+        public IItem GetItem(Point leftTop, Point rightBot, int multiplier)
+        {
+
+
+            return null;
+        }
+    }
+}
