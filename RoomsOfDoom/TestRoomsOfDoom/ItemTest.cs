1fb70c01a7df89d4acc7e414d70ca93b2a630343
Maguell <maglutila@gmail.com>
2015-05-08 22:29:45 +0200

    Testing scroll

--- RoomsOfDoom/TestRoomsOfDoom/ItemTest.cs
+++ RoomsOfDoom/TestRoomsOfDoom/ItemTest.cs
@@ -2,6 +2,7 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using RoomsOfDoom;
 using RoomsOfDoom.Items;
+using System.Collections.Generic;
 
 namespace TestRoomsOfDoom
 {
@@ -15,7 +16,7 @@ namespace TestRoomsOfDoom
         }
 
         [TestMethod]
-        public void TestGlyphs()
+        public void GlyphsTest()
         {
             Potion p = new Potion();
             Assert.IsTrue(p.Glyph == '1');
@@ -32,9 +33,25 @@ namespace TestRoomsOfDoom
             Assert.IsTrue(k.Glyph == '>');
         }
 
-        public void TestDuration()
+        [TestMethod]
+        public void MagicScrollUseTest()
         {
+            GameManager g = new RoomsOfDoom.GameManager(false);
+            MagicScroll m = new MagicScroll(new NotSoRandom(1.0), g);
+            DungeonCreator d = new DungeonCreator(random);
+            Dungeon dungeon = d.CreateDungeon(1, 10, 15);
+            g.InitRoom(dungeon.nodes[0]);
+            int size = dungeon.nodes.Count;
+            Player p = new Player();
+            m.Use(p, dungeon);
+            Assert.IsTrue(size > dungeon.nodes.Count);
 
+            List<Node> nodes = new List<Node>();
+            nodes.Add(new Node(random, 0, 15, true));
+            dungeon = new Dungeon(random, nodes, 1, 15);
+            g.InitRoom(dungeon.nodes[0]);
+            m.Use(p, dungeon);
+            Assert.IsTrue(dungeon.nodes.Count == 1);
         }
     }
 }
